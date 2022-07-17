d1d8f5237e02db0356dedd336ce0e0d1c23c5723
schwarzwald0906 <93188453+schwarzwald0906@users.noreply.github.com>
2022-07-17 12:58:11 +0900

    イベントの発生のさせ方及びスタイルの当て方

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const App = () => {
+  const onClickButton = () => alert();
+  const styleSheet = {
+    color: `blue`,
+    font: `16px`
+  };
   return (
     <>
-      <h1>こんにちは！</h1>
-      <p>お元気ですか？</p>
+      <h1 style={{ color: `red` }}>こんにちは！</h1>
+      <p style={styleSheet}>お元気ですか？</p>
+      <button onClick={onClickButton}>ボタン</button>
     </>
   );
 };
